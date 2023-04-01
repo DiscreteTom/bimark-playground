19f3b7496ca9e5f77ed917e2dd005d4b671c7d16
DiscreteTom <discrete_tom@outlook.com>
2023-04-01 15:53:47 +0800

    fix: vue router default route

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { createApp } from "vue";
 import { registerPlugins } from "@/plugins";
 import * as VueRouter from "vue-router";
 
+const app = createApp(App);
+
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHistory(),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      component: app,
+    },
+  ],
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       const id = to.hash.slice(1);
@@ -25,8 +32,6 @@ const router = VueRouter.createRouter({
     return { top: 0 };
   },
 });
-
-const app = createApp(App);
 
 registerPlugins(app);
 app.use(router);
